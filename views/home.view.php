919716f32765d98cb658ca6a7cc3c600bb023906
coa989 <a.marjanovic989@gmail.com>
2020-05-12 19:01:17 +0200

    added search

--- views/home.view.php
+++ views/home.view.php
@@ -3,7 +3,21 @@ session_start();
 include 'partials/header.php'; 
 if(isset($_SESSION['user_id'])):
 ?>
-<h3 class="text-center">Welcome <?= $_SESSION['user_name']; ?>!</h3>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+			<div class="card-body">
+				<h5 class="card-title text-center">Search Users</h5>
+				<form action="/search.php" method="get">
+					<div class="form-label-group">
+						<input type="text" name="search" class="form-control">
+						<button class="btn btn-dark btn-block text-uppercase mt-1" type="submit">Search</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
 <?php 
 else:
 	header('Location: login.view.php');
