5ee451a7c5a3343dc24dcee300019a398aaaf22f
coa989 <a.marjanovic989@gmail.com>
2020-05-14 21:19:52 +0200

    init commit

--- config.example.php
+++
@@ -1,5 +0,0 @@
-<?php  
-define('DB_HOST', ''); // host example localhost
-define('DB_USER', ''); // database username 
-define('DB_PASS', ''); // database password
-define('DB_NAME', ''); // database name
