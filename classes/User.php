fe5efd501891415b09d2e94a94c661f161a8ff10
coa989 <a.marjanovic989@gmail.com>
2020-05-26 17:38:18 +0200

    refactor

--- classes/User.php
+++ classes/User.php
@@ -9,62 +9,20 @@ class User
 		$this->db = new Database;
 	}
 
-	public function register($username, $email, $password)
+	public function showAll()
 	{
-		$this->db->query("INSERT INTO users(username, email, password) VALUES(:username, :email, :password)");
-		$this->db->bind(':username', $username);
-		$this->db->bind(':email', $email);
-		$this->db->bind(':password', $password);
-		$this->db->execute();
-		header('Location: views/login.view.php');
+		$this->db->query("SELECT * FROM users");
+		$result = $this->db->resultSet(PDO::FETCH_OBJ);
+		return $result;
 	}
 
-	public function login($email, $password)
+	public function show($id)
 	{
-		$this->db->query('SELECT * FROM users WHERE email = :email');
-    	$this->db->bind(':email', $email);
-   	 	$this->db->execute();
-    	$row = $this->db->single(PDO::FETCH_OBJ);
-
-    	if($row){
-      		$hashed_password = $row->password;
-      		if(password_verify($password, $hashed_password)){
-        		$_SESSION['user_id'] = $row->id;
-        		$_SESSION['user_name'] = $row->username;
-        		$_SESSION['user_email'] = $row->email;
-        		header('Location: views/home.view.php');
-        		return $row;
-      		} else{
-        			return false;
-      			}
-    	}
+		$this->db->query('SELECT * FROM users WHERE id = :id');
+		$this->db->bind(':id', $id);
+		return $this->db->single();
 	}
 
-	public function findByEmail($email)
-	{
-		$this->db->query('SELECT * FROM users WHERE email = :email');
-		$this->db->bind(':email', $email);
-		$this->db->execute();
-
-		if($this->db->rowCount() > 0){
-			return true;
-		} else{
-				return false;
-			}
-	}
-
-	public function findByUsername($username)
-	{
-		$this->db->query('SELECT * FROM users WHERE username = :username');
-		$this->db->bind(':username', $username);
-		$this->db->execute();
-
-		if($this->db->rowCount() > 0){
-			return true;
-		} else{
-				return false;
-			}
-	}
 
 	public function searchUser($search)
 	{
@@ -75,13 +33,6 @@ class User
 		return $result;
 	}
 
-	public function logout()
-	{
-		session_start();
-		unset($_SESSION['user_id']);
-		unset($_SESSION['user_name']);
-		unset($_SESSION['user_email']);
-		session_destroy();
-	}
+	
 
 }
