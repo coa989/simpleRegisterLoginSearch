85f5d20214e59696508a8d3264a0adfbd023bae0
coa989 <a.marjanovic989@gmail.com>
2020-05-26 17:39:51 +0200

    init commit

--- search.php
+++ search.php
@@ -1,14 +1,11 @@
 <?php 
 session_start();
-require_once 'classes/User.php';
-$user = new User;
-$result = $user->searchUser($_GET['search']);
-if(!empty($_GET['search']))
-{
-	$_SESSION['result'] = $result;
+
+if(!empty($_GET['search'])){
+	$_SESSION['users'] = $users;
 	header('Location: views/result.view.php');
-} else
-	{
+	
+} else{
 		$_SESSION['err_msg'] = 'You must enter at least 1 character.';
 		header('Location: views/home.view.php');
 	}
