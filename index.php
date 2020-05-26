949541a88f95202bc8a41f4ad0659e3bdb62d53e
coa989 <a.marjanovic989@gmail.com>
2020-05-26 17:38:37 +0200

    refactor

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-header('Location: /views/home.view.php');
- 
+session_start();
+if(isset($_SESSION['user_id'])){
+	require_once 'classes/User.php';
+	$users = (new User)->showAll();
+	$_SESSION['users'] = $users;
+	header('Location: /views/home.view.php');
+} else {
+	header('Location: /views/login.view.php');
+ }
