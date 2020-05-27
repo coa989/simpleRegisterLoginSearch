f120d70b267926dd8698dbea56516a1d89b7f5d4
coa989 <a.marjanovic989@gmail.com>
2020-05-27 14:49:40 +0200

    added methods

--- classes/User.php
+++ classes/User.php
@@ -23,8 +23,30 @@ class User
 		return $this->db->single();
 	}
 
+	public function edit($data)
+	{
+		$this->db->query("UPDATE users SET admin = :admin, username = :username, email = :email, password = :password, created_at = :created_at WHERE id = :id");
+		$this->db->bind(':id', $data['id']);
+		$this->db->bind(':admin', $data['admin']);
+		$this->db->bind(':username', $data['username']);
+		$this->db->bind(':email', $data['email']);
+		$this->db->bind(':password', $data['password']);
+		$this->db->bind(':created_at', $data['created_at']);
+		$this->db->execute();
+		header('Location: views/admin/show_user.view.php?id='. $data['id']);
+	}
 
-	public function searchUser($search)
+	public function delete($id)
+	{
+		$this->db->query("DELETE FROM users WHERE id = :id");
+		$this->db->bind(':id', $id);
+		$this->db->execute();
+		header('Location: views/home.view.php');
+
+	}
+
+
+	public function search($search)
 	{
 		$this->db->query("SELECT * FROM users WHERE username LIKE :username OR email LIKE :email");
 		$this->db->bind(':username', '%'.$search.'%');
