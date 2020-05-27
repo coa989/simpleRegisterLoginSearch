6dd34df2750b917d2049b24e26a76feed175b0a5
coa989 <a.marjanovic989@gmail.com>
2020-05-27 14:52:17 +0200

    refactor

--- views/result.view.php
+++ views/result.view.php
@@ -3,29 +3,32 @@ session_start();
 include 'partials/header.php';
 if(!empty($_SESSION['result'])): 
 ?>
-<table class="table">
-	<thead>
-		<tr>
-			<th scope="col">User ID:</th>
-			<th scope="col">Username:</th>
-			<th scope="col">Email:</th>
-			<th scope="col">Created at:</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php foreach($_SESSION['result'] as $user): ?>
-		<tr>
-			<th scope="row"><?= $user->id; ?></th>
-			<td><?= $user->username; ?></td>
-			<td><?= $user->email; ?></td>
-			<td><?= $user->created_at; ?></td>
-		</tr>
-			<?php endforeach; ?>
-	</tbody>
-</table>
+<div class="container">
+	<div class="row">
+		<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+			<div class="card-body">
+				<h5 class="card-title text-center">Results:</h5>
+				<?php foreach ($_SESSION['result'] as $user): ?>
+					<ul class="list-group">
+						<?php if($_SESSION['admin']): ?>
+						<li class="list-group-item">
+							<a href="admin/show_user.view.php?id=<?= $user->id; ?>"><?= $user->username; ?></a>
+						</li>
+						<?php else: ?>
+						<li class="list-group-item">
+							<?= $user->username; ?>
+						</li>
+						<?php endif; ?>
+					</ul>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</div>
+</div>
 <?php else: ?>
 	<h5 class="card-title text-center mt-3">No users found!</h5>
 <?php 
 endif;
+unset($_SESSION['err_msg']);
 include 'partials/footer.php'; 
 ?>
