95d05f5a279f64086ab80af2b84dd3bdd47d39da
coa989 <a.marjanovic989@gmail.com>
2021-01-05 20:45:01 +0100

    added username link

--- views/result.view.php
+++ views/result.view.php
@@ -3,26 +3,12 @@ session_start();
 include 'partials/header.php';
 if(!empty($_SESSION['result'])): 
 ?>
-<table class="table">
-	<thead>
-		<tr>
-			<th scope="col">User ID:</th>
-			<th scope="col">Username:</th>
-			<th scope="col">Email:</th>
-			<th scope="col">Created at:</th>
-		</tr>
-	</thead>
-	<tbody>
 		<?php foreach($_SESSION['result'] as $user): ?>
-		<tr>
-			<th scope="row"><?= $user->id; ?></th>
-			<td><?= $user->username; ?></td>
-			<td><?= $user->email; ?></td>
-			<td><?= $user->created_at; ?></td>
-		</tr>
+	<ul>
+		<li><a href="../show.php?id=<?= $user->id ?>"><?= $user->username; ?></a></li>
+	</ul>
 			<?php endforeach; ?>
-	</tbody>
-</table>
+
 <?php else: ?>
 	<h5 class="card-title text-center mt-3">No users found!</h5>
 <?php 
