cbef434e6ed37daa91fcfadab46ff863ed26188f
coa989 <a.marjanovic989@gmail.com>
2021-01-05 20:43:10 +0100

    added role row

--- database.sql
+++ database.sql
@@ -2,6 +2,7 @@ CREATE TABLE `yourdbname`.`users` (
 	`id` INT NOT NULL AUTO_INCREMENT , 
 	`username` VARCHAR(30) NOT NULL , 
 	`email` VARCHAR(30) NOT NULL , 
-	`password` VARCHAR(255) NOT NULL , 
+	`password` VARCHAR(255) NOT NULL ,
+    `role` VARCHAR(30) NOT NULL DEFAULT user, 
 	`created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)
 	) ENGINE = InnoDB;
