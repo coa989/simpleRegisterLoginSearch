d322808e95eb203f4043257677f8cfb1d0eced93
coa989 <a.marjanovic989@gmail.com>
2021-01-09 13:51:48 +0100

    added container

--- views/show.view.php
+++ views/show.view.php
@@ -3,24 +3,26 @@ session_start();
 include 'partials/header.php';
 
 ?>
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">User ID:</th>
-            <th scope="col">Username:</th>
-            <th scope="col">Email:</th>
-            <th scope="col">Created at:</th>
-        </tr>
-    </thead>
-    <tbody>
-        <a href="<?= ($_SESSION['role'] == 'admin') ? 'admin.view.php' : 'result.view.php' ?>"><button type="button" class="btn btn-secondary">Back</button></a>
-        <tr>
-            <th scope="row"><?= $_SESSION['user']->id; ?></th>
-            <td><?= $_SESSION['user']->username; ?></td>
-            <td><?= $_SESSION['user']->email; ?></td>
-            <td><?= $_SESSION['user']->created_at; ?></td>
-        </tr>
-    </tbody>
-</table>
+<div class="container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">User ID:</th>
+                <th scope="col">Username:</th>
+                <th scope="col">Email:</th>
+                <th scope="col">Created at:</th>
+            </tr>
+        </thead>
+        <tbody>
+            <a href="<?= ($_SESSION['role'] == 'admin') ? 'admin.view.php' : 'result.view.php' ?>"><button type="button" class="btn btn-secondary mt-1 mb-1">Back</button></a>
+            <tr>
+                <th scope="row"><?= $_SESSION['user']->id; ?></th>
+                <td><?= $_SESSION['user']->username; ?></td>
+                <td><?= $_SESSION['user']->email; ?></td>
+                <td><?= $_SESSION['user']->created_at; ?></td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 <?php
 include 'partials/footer.php';
