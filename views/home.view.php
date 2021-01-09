012deb2957d71d09b529aef0a786ba3d48d9fa93
coa989 <a.marjanovic989@gmail.com>
2021-01-09 13:51:29 +0100

    added container

--- views/home.view.php
+++ views/home.view.php
@@ -7,14 +7,7 @@ if(isset($_SESSION['user_id'])):
 	<div class="row">
 		<div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
 			<div class="card-body">
-				<h5 class="card-title text-center">Search Users</h5>
-				<form action="/search.php" method="get">
-					<div class="form-label-group">
-						<input type="text" name="search" class="form-control <?= (!empty($_SESSION['err_msg'])) ? 'is-invalid' : ''; ?>">
-						<span class="invalid-feedback"><?= !empty($_SESSION['err_msg']) ? $_SESSION['err_msg'] : ''; ?></span>
-						<button class="btn btn-dark btn-block text-uppercase mt-1" type="submit">Search</button>
-					</div>
-				</form>
+				<h5 class="card-title text-center">Welcome <?= $_SESSION['user_name']; ?>!</h5>
 			</div>
 		</div>
 	</div>
