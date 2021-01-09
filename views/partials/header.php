f253a7bb785a77e0099f325f2017fffc3cdfea9d
coa989 <a.marjanovic989@gmail.com>
2021-01-09 13:50:23 +0100

    added search bar

--- views/partials/header.php
+++ views/partials/header.php
@@ -6,36 +6,46 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <div class="collapse navbar-collapse" id="navbarsExample07">
-		  	<?php if(isset($_SESSION['user_id'])):?>
-	         	<ul class="navbar-nav mr-auto">
-		      		<li class="nav-item">
-		          		<a class="nav-link" href="<?= ($_SESSION['role'] == 'admin') ? '/views/admin.view.php' : '/views/home.view.php' ?>">Home</a>
-		        	</li>
-		        	<li class="nav-item dropdown">
-        				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        					<?= $_SESSION['user_name']; ?>
-				        </a>
-				        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-				          <a class="dropdown-item" href="/logout.php">Logout</a>
-				        </div>
-      				</li>
-      			</ul>
-	       	<?php else: ?>
-	          	<ul class="navbar-nav mr-auto">
-	      			<li class="nav-item">
-	          			<a class="nav-link" href="/views/login.view.php">Login</a>
-	        		</li>
-	        		<li class="nav-item">
-	          			<a class="nav-link" href="/views/register.view.php">Register</a>
-	      	  		</li>
-         	 	</ul>
-          	<?php endif; ?>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <div class="container">
+    <?php if(isset($_SESSION['user_id'])):?>
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="<?= ($_SESSION['role'] == 'admin') ? '/views/admin.view.php' : '/views/home.view.php' ?>">Home</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <?= $_SESSION['user_name']; ?>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="/logout.php">Logout</a>
         </div>
-      </div>
-    </nav>
+    </li>
+    </ul>
+    <form action="/search.php" method="get" class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <?php else: ?>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/views/login.view.php">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/views/register.view.php">Register</a>
+                    </li>
+                </ul>
+            <?php endif; ?>
+            </div>
+  </div>
+</nav>
 
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
